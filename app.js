1f649c3d4efeec3a08358eacf2afb81ea63d9dca
Damira Ibragimova <idamira@Damiras-MBP.home>
2016-10-30 19:18:30 -0400

    Added auth and users routes, connected files to app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const logger = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 
 // routs here
 const homePageRoute = require('./routes/index');
@@ -9,6 +11,7 @@ const loginPageRoute = require('./routes/login');
 const signupPageRoute = require('./routes/signup');
 
 const app = express();
+const SECRET = 'tacos3000';
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
@@ -21,6 +24,15 @@ app.use('/', homePageRoute);
 app.use('/login', loginPageRoute);
 app.use('/signup', signupPageRoute);
 
+
+// This is how we read the cookies sent over from the browser
+app.use(cookieParser());
+
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: SECRET
+}));
 
 app.listen(port, () => console.log('now listening to port', port));
 
